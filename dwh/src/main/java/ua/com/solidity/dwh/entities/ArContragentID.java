d59d0efb690f596e02c4af41d12c0c9a3a0565b3
BIELOIENKO Valerii Oleksandrovych <BieloienkoV@raiffeisenbank.com.ua>
2022-06-30 21:58:33 +0300

    v4.2release-fix dwh v3

--- dwh/src/main/java/ua/com/solidity/dwh/entities/ArContragentID.java
+++ dwh/src/main/java/ua/com/solidity/dwh/entities/ArContragentID.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.Hibernate;
 
 import javax.persistence.Column;

65f10885d374bcf3e6254d9220a3a006920b4c56
BieloienkoV <valerii.bieloienko@otpbank.com.ua>
2022-10-26 12:32:45 +0300

    v5.1.303

--- web/src/main/java/ua/com/solidity/web/service/RoleService.java
+++ web/src/main/java/ua/com/solidity/web/service/RoleService.java
@@ -18,20 +18,20 @@ public class RoleService {
 	private final RoleMapRepository roleMapRepository;
 
 	public Role getRoleFromMemberOf(ArrayList<String> memberOf) {
-		System.out.println('Login attempt -> ' + memberOf.toString())
+		System.out.println('Login attempt -> ' + memberOf.toString());
 		List<String> personGroups = memberOf
 				.stream()
 				.map((s) -> s.substring(s.indexOf("=") + 1, s.indexOf(",")))
 				.collect(Collectors.toList());
 		List<RoleMap> roleMaps = roleMapRepository.findAllById(personGroups);
-		System.out.println('Role maps ' + roleMaps.toString())
+		System.out.println('Role maps ' + roleMaps.toString());
 		Role role = null;
 
 		if (!roleMaps.isEmpty()) {
 			roleMaps.sort(Comparator.comparingInt(i -> i.getRole().getId()));
 			role = roleMaps.get(0).getRole();
 		}
-		System.out.println('Role -> ' + role.toString())
+		System.out.println('Role -> ' + role.toString());
 		return role;
 	}
 }
